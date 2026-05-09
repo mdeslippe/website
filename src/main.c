3cc1b3de14b19f3cc8b70073334ee06e6a3bf7bd
Myles Deslippe <mdeslippe76@gmail.com>
2026-05-09 12:06:22 -0400

    #1: Added a minimal c program

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * @brief Application entry point.
+ *
+ * @param argc Count of execution arguments.
+ * @param argv Null-terminated array of execution argument strings.
+ * @return EXIT_SUCCESS on success; otherwise an implementation-defined error code.
+ */
+int main(int argc, char const* argv[]) {
+    /* status code */
+    return EXIT_SUCCESS;
+}
