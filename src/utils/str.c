94844085373bf6bf21c522af10dd9109d8692234
Myles Deslippe <mdeslippe76@gmail.com>
2026-05-10 12:53:25 -0400

    #3: Added debug validation assertions

--- src/utils/str.c
+++ src/utils/str.c
@@ -1,6 +1,29 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include "str.h"
+
+/**
+ * @brief Asserts that a string is valid.
+ *
+ * This macro is intended for internal debug assertions to verify that a string 
+ * satisfies all required invariants.
+ *
+ * A valid string must satisfy:
+ * - `string != NULL`
+ * - `string->data != NULL`
+ * - `string->capacity > 0`
+ * - `string->length < string->capacity`
+ * - `string->data[string->length] == '\0'`
+ */
+#define STRING_ASSERT_VALID(string)              \
+    assert(                                      \
+        (string) != NULL &&                      \
+        (string)->data != NULL &&                \
+        (string)->capacity > 0 &&                \
+        (string)->length < (string)->capacity && \
+        (string)->data[(string)->length] == '\0' \
+    )
 
 StringResult string_init(String* string) {
 
@@ -18,6 +41,8 @@ StringResult string_init(String* string) {
     string->length = 0;
     string->data = data;
     string->data[string->length] = '\0';
+
+    STRING_ASSERT_VALID(string);
 
     return STRING_SUCCESS;
 
@@ -47,6 +72,8 @@ StringResult string_reserve(String* string, size_t capacity) {
         return STRING_ERROR_ARGUMENT;
     }
 
+    STRING_ASSERT_VALID(string);
+
     if (capacity <= string->capacity) {
       return STRING_SUCCESS;
     }
@@ -70,6 +97,8 @@ StringResult string_reserve(String* string, size_t capacity) {
 
     string->data = data;
     string->capacity = new_capacity;
+
+    STRING_ASSERT_VALID(string);
 
     return STRING_SUCCESS;
 
