3e0938910decad7e406db10f38ea9d474eabd390
Myles Deslippe <mdeslippe76@gmail.com>
2026-07-03 12:57:26 -0400

    #23: Implemented linked list init and free functions

--- src/utils/linked_list.c
+++ src/utils/linked_list.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdlib.h>
 #include "linked_list.h"
 
 /**
@@ -14,6 +15,8 @@
  * - `list->head->previous == NULL` when `list->head != NULL`
  * - `list->tail->next == NULL` when `list->tail != NULL`
  * - `list->head == list->tail` when `list->length == 1`
+ *
+ * @remark A zero-initialized linked list is considered valid.
  */
 #define LINKED_LIST_ASSERT_VALID(list)                              \
     assert(                                                         \
@@ -24,3 +27,46 @@
         ((list)->tail == NULL || (list)->tail->next == NULL) &&     \
         ((list)->length != 1 || (list)->head == (list)->tail)       \
     )
+
+LinkedListResult linked_list_init(LinkedList* linked_list) {
+
+    if (linked_list == NULL) {
+        return LINKED_LIST_ERROR_ARGUMENT;
+    }
+
+    linked_list->length = 0;
+    linked_list->head = NULL;
+    linked_list->tail = NULL;
+
+    LINKED_LIST_ASSERT_VALID(linked_list);
+
+    return LINKED_LIST_SUCCESS;
+
+}
+
+LinkedListResult linked_list_free(LinkedList* linked_list) {
+
+    if (linked_list == NULL) {
+        return LINKED_LIST_ERROR_ARGUMENT;
+    }
+
+    LINKED_LIST_ASSERT_VALID(linked_list);
+
+    LinkedNode* current = linked_list->head;
+    LinkedNode* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    linked_list->length = 0;
+    linked_list->head = NULL;
+    linked_list->tail = NULL;
+
+    LINKED_LIST_ASSERT_VALID(linked_list);
+
+    return LINKED_LIST_SUCCESS;
+
+}
